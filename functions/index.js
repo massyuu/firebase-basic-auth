c1d79fa790f992ef0b5300164fbd1c9dcd6d2b89
massyuu <massyuu.dear85y@gmail.com>
2019-06-17 13:27:19 +0900

    basic認証処理追加

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,19 @@
-const functions = require('firebase-functions');
+/*
+    前準備：firebase.jsonにて、
+    デフォルトのホスト先（public）ではなく、firebaseBasicAuthを参照するようrewritesしておくこと
+ */
+const functions = require('firebase-functions')
+const express = require('express')
+const basicAuth = require('basic-auth-connect')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Basic認証のID、PASS設定
+const USERNAME = 'user'
+const PASSWORD = 'pass'
+
+// Basic Auth
+const app = express()
+app.use(basicAuth(USERNAME, PASSWORD))
+
+// functions の dist を読み込むよう設定
+app.use(express.static(__dirname + '/dist/'))
+exports.firebaseBasicAuth = functions.https.onRequest(app)
---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    // ビルド出力先の変更
+    outputDir: 'functions/dist',
+    // ビルドインの設定
+    devServer: {
+        port: 8888,
+        disableHostCheck: true,
+    },
+}
